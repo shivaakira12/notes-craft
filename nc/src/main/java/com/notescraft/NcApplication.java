737a75a8402fc93a6f5e90e715ae21c8c44fefaf
Shiva <63032037+shivaakira12@users.noreply.github.com>
2025-01-21 22:30:02 +0530

    Add files via upload

---
+++ nc/src/main/java/com/notescraft/NcApplication.java
@@ -0,0 +1,13 @@
+package com.notescraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcApplication.class, args);
+	}
+
+}
---
+++ nc/src/test/java/com/notescraft/NcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notescraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
